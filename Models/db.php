794836dc0db9854c0929f59926739c45c38d161c
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-04 16:47:23 +0200

    aggiornati i file in cartella

---
+++ Models/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$generi = ['Fantascienza - ', 'Fantastico - ', 'Commedia'];
+
+// $movie_1 = new Movie('Ritorno al futuro', 'Fantascienza', 9.5);
+// $movie_2 = new Movie('Harry Potter', 'Fantastico', 10);
+
+$movie_1 = new Movie('Ritorno al futuro', $generi, 9.5);
+$movie_2 = new Movie('Harry Potter', $generi, 10);
--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
 require __DIR__ . '/Models/Movie.php';
-
-$generi = ['Fantascienza - ', 'Fantastico - ', 'Commedia'];
-
-// $movie_1 = new Movie('Ritorno al futuro', 'Fantascienza', 9.5);
-// $movie_2 = new Movie('Harry Potter', 'Fantastico', 10);
-
-$movie_1 = new Movie('Ritorno al futuro', $generi, 9.5);
-$movie_2 = new Movie('Harry Potter', $generi, 10);
-
+require __DIR__ . '/Models/db.php';
 ?>
 
 <!DOCTYPE html>
